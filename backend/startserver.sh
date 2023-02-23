0e0022aebf85ed37a535103b2bf4482cce35e853
k4Y53N <51146583+k4Y53N@users.noreply.github.com>
2023-02-23 13:24:01 +0800

    add requirement package and change startserver.sh \r\n -> \n

--- backend/startserver.sh
+++ backend/startserver.sh
@@ -2,4 +2,5 @@
 python3 manage.py collectstatic --noinput
 python3 manage.py makemigrations
 python3 manage.py migrate --run-sync
-python3 manage.py runserver 0.0.0.0:${PORT}
+# python3 manage.py runserver 0.0.0.0:${PORT}
+gunicorn base.wsgi
